6a95f5393b4ea1fd356766f5d22c9b33e77d8e8c
msevigny <m.sevigny@umontreal.ca>
2024-05-29 11:41:22 -0400

    Traitement des données synchro dans le pilote quotidien

--- utils/pilote_quotidien.py
+++ utils/pilote_quotidien.py
@@ -35,6 +35,65 @@ if not (os.path.exists(dossier_sortie) and os.path.isdir(dossier_sortie) and os.
 
 journee_actuelle = datetime.now().strftime("%Y-%m-%d")
 
+####################
+# Fichiers Synchro #
+####################
+
+# On vérifie d'abord si on a les fichiers Synchro de la veille
+chemin_fichier_synchro_ac = config['quotidien']['synchro_ac']
+chemin_fichier_synchro_rh = config['quotidien']['synchro_rh']
+
+# On va s'assurer qu'on a les fichiers
+if (os.path.isfile(chemin_fichier_synchro_ac) and os.path.isfile(chemin_fichier_synchro_rh)):
+    # On a les fichiers, on vérifie leur date
+    jour_synchro_ac = datetime.fromtimestamp(os.path.getmtime(chemin_fichier_synchro_ac)).date().strftime("%Y-%m-%d")
+    jour_synchro_rh = datetime.fromtimestamp(os.path.getmtime(chemin_fichier_synchro_rh)).date().strftime("%Y-%m-%d")
+    if (jour_synchro_rh == jour_synchro_ac and jour_synchro_ac == journee_actuelle):
+        # On a les bonnes dates de fichier, on peut procéder
+        # Extraction des fichiers, d'abord les étudiants
+        journee_extraction = (datetime.strptime(journee_actuelle, "%Y-%m-%d") - timedelta(days=1)).strftime("%Y-%m-%d")
+        fichier_csv_ac = f"{dossier_sortie}{os.path.sep}synchro_ac-{journee_extraction}.csv"
+        script = "extraction/etudiants.py"
+        resultat = subprocess.run(["python", script, "--fichier_entree", chemin_fichier_synchro_ac, "--fichier_sortie", fichier_csv_ac], capture_output=True, text=True)
+        if (resultat.returncode > 0):
+            # On traite l'erreur mais on ne poursuit pas ce traitement
+            logger.error("Erreur dans l'extraction des données de Synchro académique : " + resultat.stderr)
+        else:
+            # On poursuit avec le fichier du personnel
+            fichier_csv_rh = f"{dossier_sortie}{os.path.sep}synchro_rh-{journee_extraction}.csv"
+            script = "extraction/personnel.py"
+            resultat = subprocess.run(["python", script, "--fichier_entree", chemin_fichier_synchro_rh, "--fichier_sortie", fichier_csv_rh], capture_output=True, text=True)
+            if (resultat.returncode > 0):
+                # On traite l'erreur mais on ne poursuit pas ce traitement
+                logger.error("Erreur dans l'extraction des données de Synchro RH : " + resultat.stderr)
+            else:
+                # On poursuit avec le chargement des étudiants
+                journee_chargement = (datetime.strptime(journee_actuelle, "%Y-%m-%d") - timedelta(days=1)).strftime("%Y-%m-%d")
+                script = "chargement/etudiants.py"
+                resultat = subprocess.run(["python", script, "--jour", journee_chargement, "--fichier", fichier_csv_ac], capture_output=True, text=True)
+                if (resultat.returncode > 0):
+                    # On traite l'erreur mais on ne poursuit pas ce traitement
+                    logger.error("Erreur dans le chargement des données de Synchro académique : " + resultat.stderr)
+                else:
+                    # On poursuit avec le chargement du personnel
+                    script = "chargement/personnel.py"
+                    resultat = subprocess.run(["python", script, "--jour", journee_chargement, "--fichier", fichier_csv_rh], capture_output=True, text=True)
+                    if (resultat.returncode > 0):
+                        # On traite l'erreur mais on ne poursuit pas ce traitement
+                        logger.error("Erreur dans le chargement des données de Synchro RH : " + resultat.stderr)
+                    else:
+                        # On poursuit avec la transformation, étudiants et personnel
+                        script = "transformation/clienteles.py"
+                        resultat = subprocess.run(["python", script], capture_output=True, text=True)
+                        if (resultat.returncode > 0):
+                            # On traite l'erreur mais on ne poursuit pas ce traitement
+                            logger.error("Erreur dans la transformation des données de clientèles : " + resultat.stderr)
+    else:
+        logger.error("Erreur de date dans les fichiers Synchro: " + jour_synchro_ac + ", " + jour_synchro_rh + " mais " + journee_actuelle + " attendu")
+else:
+    # Les fichiers Synchro ne sont pas accessibles 
+    logger.error("Impossible de lire les fichiers Synchro " + chemin_fichier_synchro_rh + " ou " + chemin_fichier_synchro_ac)
+
 ##########################
 # Réservations de salles #
 ##########################
@@ -166,4 +225,3 @@ else:
 # TODO:
 #   - emprunts
 #   - statistiques de référence
-#   - étudiants, personnel, clientèles
