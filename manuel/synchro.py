5950127ddb41b64e65b113fffbf30c39a59b57e8
msevigny <m.sevigny@umontreal.ca>
2024-05-31 10:04:57 -0400

    Ajout du paramètre session et modification pour poursuivre le traitement en cas d'erreur

--- manuel/synchro.py
+++ manuel/synchro.py
@@ -16,6 +16,7 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='Script pour extraire, charger et transformer les données Synchro dans l\'entrepôt')
     parser.add_argument('--dossier', required=True, help='Chemin du dossier où se trouvent les fichiers Synchro à charger')
     parser.add_argument('--sortie', required=True, help='Chemin du dossier où déposer les fichiers de sortie (temporaires)')
+    parser.add_argument('--session', required=False, default=False)
     return parser.parse_args()
 
 
@@ -29,6 +30,7 @@ args = parse_arguments()
 # Chemin du fichier CSV à charger
 chemin_dossier = args.dossier
 dossier_sortie = args.sortie
+session = args.session
 
 # On boucle sur les fichiers dans le dossier
 dossier = Path(chemin_dossier)
@@ -50,31 +52,30 @@ for fichier in dossier.iterdir():
                 fichier_csv_etudiants = dossier_sortie + os.sep + nom_fichier_etudiants + ".csv"
                 fichier_csv_personnel = dossier_sortie + os.sep + nom_fichier_personnel + ".csv"
 
-                print("Traitement du fichier Synchro " + nom_fichier_etudiants)
+                print(datetime.now().isoformat() + " - Traitement du fichier Synchro " + nom_fichier_etudiants)
 
                 # Extraction des données
                 resultat = subprocess.run(["python", "extraction/etudiants.py", "--fichier_entree", fichier_etudiants, "--fichier_sortie", fichier_csv_etudiants], capture_output=True, text=True)
                 if (resultat.returncode > 0):
-                    logger.error("Erreur dans l'extraction des données de Synchro AC : " + resultat.stderr)
-                    sys.exit(1)
+                    logger.error("Erreur dans l'extraction des données de Synchro AC " + fichier_etudiants + " : " + resultat.stderr)
                 resultat = subprocess.run(["python", "extraction/personnel.py", "--fichier_entree", fichier_personnel, "--fichier_sortie", fichier_csv_personnel], capture_output=True, text=True)
                 if (resultat.returncode > 0):
-                    logger.error("Erreur dans l'extraction des données de Synchro RH : " + resultat.stderr)
-                    sys.exit(1)
+                    logger.error("Erreur dans l'extraction des données de Synchro RH " + fichier_personnel + " : " + resultat.stderr)
                 # Chargement des données
                 resultat = subprocess.run(["python", "chargement/etudiants.py", "--jour", jour_iso, "--fichier", fichier_csv_etudiants], capture_output=True, text=True)
                 if (resultat.returncode > 0):
-                    logger.error("Erreur dans le chargement des données de Synchro AC : " + resultat.stderr)
-                    sys.exit(1)
-                resultat = subprocess.run(["python", "chargement/personnel.py", "--jour", jour_iso, "--fichier", fichier_csv_personnel], capture_output=True, text=True)
+                    logger.error("Erreur dans le chargement des données de Synchro AC " + fichier_csv_etudiants + " : " + resultat.stderr)
+                resultat = subprocess.run(["python", "chargement/personnel.py", "--jour", jour_iso, "--fichier", fichier_csv_etudiants], capture_output=True, text=True)
                 if (resultat.returncode > 0):
-                    logger.error("Erreur dans le chargement des données de Synchro RH : " + resultat.stderr)
-                    sys.exit(1)
+                    logger.error("Erreur dans le chargement des données de Synchro RH " + fichier_csv_etudiants + " : " + resultat.stderr)
                 # On peut maintenant supprimer les fichiers CSV
                 os.remove(fichier_csv_etudiants)
                 os.remove(fichier_csv_personnel)
                 # Transformation des données
-                resultat = subprocess.run(["python", "transformation/clienteles.py"], capture_output=True, text=True)
+                if (session):
+                    resultat = subprocess.run(["python", "transformation/clienteles.py", "--session", jour_iso], capture_output=True, text=True)
+                else:
+                    resultat = subprocess.run(["python", "transformation/clienteles.py"], capture_output=True, text=True)
                 if (resultat.returncode > 0):
-                    logger.error("Erreur dans la transformation des données de Synchro : " + resultat.stderr)
+                    logger.error("Erreur dans la transformation des données de Synchro " + jour_iso + " : " + resultat.stderr)
                     sys.exit(1)
